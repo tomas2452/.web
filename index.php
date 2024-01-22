820c0533b7e2624feb7077a4a7b3f73632df8e09
Gerkelis Agency <156610517+tomas2452@users.noreply.github.com>
2024-01-22 20:23:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="antonio.ico">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mail</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ toto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Carrois+Gothic+SC&family=Cinzel&family=Cinzel+Decorative&family=IM+Fell+English+SC&family=Mate+SC&family=Metal+Mania&family=Playfair+Display+SC&family=Vast+Shadow&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+
+	<link rel="stylesheet" type="text/css" href="estilo9.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mail Form</title>
+</head>
+<body>
+	<form>
+<input type="text" placeholder="name" name="Name">
+<input type="email" placeholder="email" name="Mail">
+<input type="text" placeholder="asunto" name="Required service">
+<textarea placeholder="mensaje" name="msg"></textarea>
+</form>
+</body>
+</html>
